7f5e5fc70ff25b63b75fcc9d1f1b5593d137292a
imzhukov <zhukovim@yandex.ru>
2015-11-09 00:58:19 +0300

    add version

---
+++ version/revision.h
@@ -0,0 +1,3 @@
+#define version_release L"3.1.5"
+#define revision L"3.1.5.3:5M"
+#define version_build L"3:5M"
---
+++ version/version.cmd
@@ -0,0 +1,10 @@
+type define1 > revision.h
+svnversion -n .. >> revision.h
+type define2 >> revision.h
+svnversion -n .. >> revision.h
+type define3 >> revision.h
+
+type iss1 > ..\revision.iss
+svnversion -n .. >> ..\revision.iss
+
+
